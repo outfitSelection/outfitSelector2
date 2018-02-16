2e27dad4789c6aa2f98b0d89eac8dd5b676f062d
outfitSelection <36526779+outfitSelection@users.noreply.github.com>
2018-02-16 13:47:19 -0600

    Add files via upload

---
+++ headermenu.js
@@ -0,0 +1,38 @@
+function hamburger() {
+    document.getElementById("dropdown-menu").classList.toggle("show");
+}
+
+
+window.onclick = function(event1) {
+  if (!event1.target.matches('.dropdownbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-list");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+function profile() {
+    document.getElementById("dropdown-profile").classList.toggle("showProfile");
+}
+
+
+window.onclick = function(event2) {
+  if (!event2.target.matches('.profilebtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-list-profile");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('showProfile')) {
+        openDropdown.classList.remove('showProfile');
+      }
+    }
+  }
+}
