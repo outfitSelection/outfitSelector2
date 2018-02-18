212408a0da2748ca720d8f9cbc183d6030711a5c
outfitSelection <36526779+outfitSelection@users.noreply.github.com>
2018-02-17 19:32:23 -0600

    Add files via upload

--- headermenu.js
+++ headermenu.js
@@ -1,23 +1,3 @@
-function hamburger() {
-    document.getElementById("dropdown-menu").classList.toggle("show");
-}
-
-
-window.onclick = function(event1) {
-  if (!event1.target.matches('.dropdownbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-list");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-
-
 function profile() {
     document.getElementById("dropdown-profile").classList.toggle("showProfile");
 }
@@ -32,6 +12,25 @@ window.onclick = function(event2) {
       var openDropdown = dropdowns[i];
       if (openDropdown.classList.contains('showProfile')) {
         openDropdown.classList.remove('showProfile');
+      }
+    }
+  }
+}
+
+function hamburger() {
+    document.getElementById("dropdown-menu").classList.toggle("show");
+}
+
+
+window.onclick = function(event1) {
+  if (!event1.target.matches('.dropdownbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-list");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
   }
