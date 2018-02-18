93450ca833aa3e6744f39cff9544b08556ab980a
outfitSelection <36526779+outfitSelection@users.noreply.github.com>
2018-02-18 17:01:33 -0600

    Add files via upload

--- closet.js
+++ closet.js
@@ -36,9 +36,28 @@ window.onclick = function(event1) {
   }
 }
 
+var inputs = document.querySelectorAll('input');
+for (var i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener('focus', function(){
+        this.parentNode.appendChild(document.querySelector('#myUL'));
+    });
+}
+
 function searchClothes() {
+    // Declare variables
+    var input, filter, ul, li, a, i;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('p');
 
-
-
-  
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
 }
--- home.js
+++ home.js
@@ -36,3 +36,59 @@ window.onclick = function(event2) {
     }
   }
 }
+
+
+// for the addOutfit page
+
+function addOutfitConfirmation () {
+  var clothingInput, tagsInput; 
+
+  clothingInput = document.forms["add-outfit-form"]["clothing_types"].value;
+
+  if (clothingInput == ""){
+    alert("Please complete required field!!");
+    return false;
+  }
+
+  alert("Outfit added to Closet!");
+  return true;
+}
+
+
+// function showGallery() {
+//  // var gallery = document.getElementById("gallery");
+//  // gallery.style.visibility = 'visible';
+//  debugger;
+
+// }
+
+// function to upload images
+function myFunction(){
+    var x = document.getElementById("myFile");
+    var txt = "";
+    if ('files' in x) {
+        if (x.files.length == 0) {
+            txt = "Select one or more files.";
+        } else {
+            for (var i = 0; i < x.files.length; i++) {
+                txt += "<br><strong>" + (i+1) + ". file</strong><br>";
+                var file = x.files[i];
+                if ('name' in file) {
+                    txt += "name: " + file.name + "<br>";
+                }
+                if ('size' in file) {
+                    txt += "size: " + file.size + " bytes <br>";
+                }
+            }
+        }
+    } 
+    else {
+        if (x.value == "") {
+            txt += "Select one or more files.";
+        } else {
+            txt += "The files property is not supported by your browser!";
+            txt  += "<br>The path of the selected file: " + x.value; // If the browser does not support the files property, it will return the path of the selected file instead. 
+        }
+    }
+    document.getElementById("demo").innerHTML = txt;
+}
