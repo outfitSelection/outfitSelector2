c1984347f19df6afad9bafe4161071cfb2007151
Idrees Kamal <ikamal97@gmail.com>
2018-02-17 21:24:26 -0600

    Fixed bug pertaining to Add to Outfit button submitting form when required fields weren't filled out

--- addoutfit.js
+++ addoutfit.js
@@ -1,3 +1,15 @@
-function submitOutfit () {
-	confirm("Outfit added to Closet!")
+function addOutfitConfirmation () {
+	var clothingInput, tagsInput; 
+
+	clothingInput = document.forms["add-outfit-form"]["clothing_types"].value;
+	tagsInput = document.forms["add-outfit-form"]["tags"].value;
+
+	if (clothingInput == "" || tagsInput== ""){
+		alert("Form must be filled out before submitting!");
+		return false;
+	}
+
+	confirm("Outfit added to Closet!");
+	return true;
 }
+
