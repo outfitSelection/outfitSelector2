3cbac4e3306756addca2414f28f1094a7e6be1b0
outfitSelection <36526779+outfitSelection@users.noreply.github.com>
2018-02-23 01:06:48 -0600

    Add files via upload

--- closet.js
+++ closet.js
@@ -15,3 +15,22 @@ function list() {
     });
   } 
 }
+
+function searchClothes() {
+    // Declare variables
+    var input, filter, ul, li, a, i;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('button');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
