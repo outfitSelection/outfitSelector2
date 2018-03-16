5ec2f5cb5c2a4a69c2aafe12ca49f5f55a202982
Idrees Kamal <ikamal97@gmail.com>
2018-03-15 20:11:51 -0500

    added button for edit

--- index.js
+++ index.js
@@ -1,34 +1,51 @@
 $(document).ready(function(){
-    $(".suggestion").click(function(){
-      var x = this.id;
-      var s = '#' + x + ' img';
-      var i = $(s).attr('src');
-      swal({
-          title: "Wear this outfit for the day?",
+  $(".suggestion").click(function(){
+    var x = this.id;
+    var s = '#' + x + ' img';
+    var i = $(s).attr('src');
+    swal({
+      title: "Wear this outfit for the day?",
+      icon: i,
+      buttons: {
+        edit: true,
+        cancel: true,
+        OK: true,
+      },
+    })
+    .then((value) => {
+      switch (value) {
+
+        case "edit":
+        swal({
           icon: i,
-          buttons: true,
-          dangerMode: true,
-        })
-        .then((willDelete) => {
-          if (willDelete) {
-            $('.scrollmenu').children('div').each(function () {
+        });        
+        break;
+
+        case "OK":
+        if(value == "OK"){
+          $('.scrollmenu').children('div').each(
+            function () {
               if($(this).hasClass('selected')){
                 $(this).removeClass('selected');
               }
-            })
-            $(this).addClass('selected');
-            swal("Great choice!", {
-              icon: "success",
-            });
-          } else {
-            swal("No problem, pick another outfit!");
-          }
-        });
-      })
+            }
+            )
+          $(this).addClass('selected');
+          swal("Great choice!", {
+            icon: "success",
+          });
+        }
+
+        default:
+        swal("No problem, pick another outfit!");
+        break;
+      }
+    });
   })
+})
 
 function profile() {
-    document.getElementById("dropdown-profile").classList.toggle("showProfile");
+  document.getElementById("dropdown-profile").classList.toggle("showProfile");
 }
 
 document.getElementById("login").onclick = function() {
@@ -58,7 +75,7 @@ window.onclick = function(event2) {
 }
 
 function hamburger() {
-    document.getElementById("dropdown-menu").classList.toggle("show");
+  document.getElementById("dropdown-menu").classList.toggle("show");
 }
 
 
@@ -97,33 +114,33 @@ function addOutfitConfirmation () {
 
 // function to upload images
 function myFunction(){
-    var x = document.getElementById("myFile");
-    var txt = "";
-    if ('files' in x) {
-        if (x.files.length == 0) {
-            txt = "Select one or more files.";
-        } else {
-            for (var i = 0; i < x.files.length; i++) {
-                txt += "<br><strong>" + (i+1) + ". file</strong><br>";
-                var file = x.files[i];
-                if ('name' in file) {
-                    txt += "name: " + file.name + "<br>";
-                }
-                if ('size' in file) {
-                    txt += "size: " + file.size + " bytes <br>";
-                }
-            }
+  var x = document.getElementById("myFile");
+  var txt = "";
+  if ('files' in x) {
+    if (x.files.length == 0) {
+      txt = "Select one or more files.";
+    } else {
+      for (var i = 0; i < x.files.length; i++) {
+        txt += "<br><strong>" + (i+1) + ". file</strong><br>";
+        var file = x.files[i];
+        if ('name' in file) {
+          txt += "name: " + file.name + "<br>";
         }
-    } 
-    else {
-        if (x.value == "") {
-            txt += "Select one or more files.";
-        } else {
-            txt += "The files property is not supported by your browser!";
-            txt  += "<br>The path of the selected file: " + x.value; // If the browser does not support the files property, it will return the path of the selected file instead. 
+        if ('size' in file) {
+          txt += "size: " + file.size + " bytes <br>";
         }
+      }
     }
-    document.getElementById("demo").innerHTML = txt;
-}
+  } 
+  else {
+    if (x.value == "") {
+      txt += "Select one or more files.";
+    } else {
+      txt += "The files property is not supported by your browser!";
+            txt  += "<br>The path of the selected file: " + x.value; // If the browser does not support the files property, it will return the path of the selected file instead. 
+          }
+        }
+        document.getElementById("demo").innerHTML = txt;
+      }
 
 
