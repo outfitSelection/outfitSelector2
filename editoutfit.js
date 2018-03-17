3ec6180610ec17d171b23335522930157ccfac92
Idrees Kamal <ikamal97@gmail.com>
2018-03-16 23:23:58 -0500

    finished

--- editoutfit.js
+++ editoutfit.js
@@ -3,21 +3,42 @@ $(document).ready(function(){
 		var x = this.id;
 		var s = '#' + x + ' img';
 		var i = $(s).attr('src');
-		console.log(i);
 
 		document.getElementById("preview-image-container").innerHTML = "<img src=" + i + ">";
-		console.log(document.getElementById("preview-image-container").innerHTML)
-	});
 
-	$(".wear_btn").click(function(){
-		var x = 'preview-image-container';
-		var s = '#' + x + ' img';
-		var i = $(s).attr('src');
-		console.log(i);
-		swal({
-			title: "Wear this outfit for the day?",
-			icon: i,
-			button: true,
+		$('.scrollmenu').children('div').each(function () {
+			if($(this).hasClass('selected')){
+				$(this).removeClass('selected');
+			}
+		})
+		$(this).addClass('selected');
+		swal("Great choice!", {
+			icon: "success",
 		});
 	});
+
+	// $(".wear_btn").click(function(){
+	// 	var x = 'preview-image-container';
+	// 	var s = '#' + x + ' img';
+	// 	var i = $(s).attr('src');
+	// 	console.log(i);
+	// 	swal({
+	// 		title: "Wear this outfit for the day?",
+	// 		icon: i,
+	// 		button: true,
+	// 	})
+	// 	.then((value) => {
+	// 		if (value) {
+	// 			$('.scrollmenu').children('div').each(function () {
+	// 				if($(this).hasClass('selected')){
+	// 					$(this).removeClass('selected');
+	// 				}
+	// 			})
+	// 			$(this).addClass('selected');
+	// 			swal("Great choice!", {
+	// 				icon: "success",
+	// 			});
+	// 		}
+	// 	})
+	// });
 });
